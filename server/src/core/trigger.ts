53de4c735880d1972ddd7b875e133b5cf2b2694e
Andy Knotts <arknotts@gmail.com>
2016-10-20 21:28:02 -0400

    Add PiGpioTrigger
    This is the real trigger implementation that talks to the relays connected to the Pi's GPIO pins.

--- server/src/core/trigger.ts
+++ server/src/core/trigger.ts
@@ -1,6 +1,30 @@
+var gpio = require('rpi-gpio');
+
 export interface ITrigger {
     start(): void;
     stop(): void;
+}
+
+class PiGpioTrigger implements ITrigger {
+    constructor(private _outPin: number) {
+        gpio.setup(_outPin, gpio.DIR_OUT, (err) => {
+            if(err) throw `Error connecting to pin ${_outPin}: ${err}`;
+        });
+    }
+
+    private writeToPin(val: boolean) {
+        gpio.write(this._outPin, val, function(err) {
+            if (err) throw `Error writing to pin ${this._outPin}: ${err}`;
+        });
+    }
+
+    start() {
+        this.writeToPin(true);
+    }
+
+    stop() {
+        this.writeToPin(false);
+    }
 }
 
 export class FurnaceTrigger implements ITrigger {
