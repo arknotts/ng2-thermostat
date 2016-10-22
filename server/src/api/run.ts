663592e821254f4993cb84b058d8a47a325b65c8
Andy Knotts <arknotts@gmail.com>
2016-10-22 12:42:36 -0400

    Default to rest server

--- server/src/api/run.ts
+++ server/src/api/run.ts
@@ -1,5 +1,7 @@
 import { BaseServer } from './baseServer';
+import { RestServer } from './restServer';
 import { SimServer } from './simServer';
 
-let server = new SimServer();
+//let server = new SimServer();
+let server = new RestServer();
 server.start();
