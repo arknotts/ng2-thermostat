4901a18bc2649d913359866c5c0d476c07bda2ec
arknotts <arknotts@gmail.com>
2017-05-06 07:28:43 -0400

    Add one schedule per mode
    Previously one schedule was used for both heating and cooling. In practice, this isn't ideal as the schedules for each of these modes will almost always be different. This adds the ability to have a separate schedule for both heating and cooling. The scheduler now takes a schedule object that contains both heating/cooling schedules and the server passes in the correct mode to pick. The server watches for mode changes and re-initializes the schedule when this happens.

--- server/src/api/schedule.ts
+++ server/src/api/schedule.ts
@@ -1,26 +1,43 @@
 import * as cron from 'cron';
 
-import { IScheduleConfiguration, IScheduleItem } from './configuration';
+import { IScheduleConfiguration, IScheduleItem, IWeeklySchedule } from './configuration';
+import { ThermostatMode } from "../../../common/thermostatMode";
 
 export interface IScheduler {
-	initSchedule: {(callback: {(temperature)})};
+	initSchedule: {(mode: ThermostatMode, callback: {(temperature)})};
 }
 
 export class Scheduler implements IScheduler {
-	constructor(private _schedule: IScheduleConfiguration) {}
+	private _currentWeeklySchedule: IWeeklySchedule;
+	private _cronJobs: cron.CronJob[];
 
-	initSchedule(callback: {(temperature): void}) {
-		this._schedule.weekdays.forEach((item) => {
+	constructor(private _schedule: IScheduleConfiguration) {
+		this._cronJobs = [];
+	}
+
+	initSchedule(mode: ThermostatMode, callback: {(temperature): void}) {
+		this.clearExistingJobs();
+		this._currentWeeklySchedule = mode === ThermostatMode.Heating ?
+							this._schedule.heating : this._schedule.cooling;
+
+		this._currentWeeklySchedule.weekdays.forEach((item) => {
 			this.initCronJob(item, 'MON-FRI', callback);
 		});
 
-		this._schedule.weekends.forEach((item) => {
+		this._currentWeeklySchedule.weekends.forEach((item) => {
 			this.initCronJob(item, 'SAT,SUN', callback);
 		});
 	}
 
+	private clearExistingJobs() {
+		this._cronJobs.forEach((cronJob) => {
+			cronJob.stop();
+		});
+		this._cronJobs = [];
+	}
+
 	private initCronJob(item: IScheduleItem, days: string, callback: {(temperature): void}) {
-		new cron.CronJob({
+		let cronJob = new cron.CronJob({
 			cronTime: this.buildCronString(item.time, days),
 			onTick: () => {
 				callback(item.temperature);
@@ -28,6 +45,8 @@ export class Scheduler implements IScheduler {
 			start: true,
 			timeZone: this._schedule.timezone
 		});
+
+		this._cronJobs.push(cronJob);
 	}
 	
 	private buildCronString(strTime: string, dayOfWeek: string) {
